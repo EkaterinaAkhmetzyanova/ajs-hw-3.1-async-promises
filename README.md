ae821f7974854ec560ad3723b324b8cce05574bb
Ekaterina Akhmetzyanova <a.ekaterina323@gmail.com>
2021-05-15 18:33:17 +0300

    promises task

---
+++ README.md
@@ -0,0 +1,2 @@
+Домашнее задание к лекции «Promises, async/await». Promises 
+[![Build status](https://ci.appveyor.com/api/projects/status/5fygvoofiu900wx8?svg=true)](https://ci.appveyor.com/project/EkaterinaAkhmetzyanova/ajs-hw-3-1-async-promises)
---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-console */
+import GameSavingLoader from './GameSavingLoader';
+
+GameSavingLoader.load().then((saving) => {
+  // saving объект класса GameSaving
+  console.log(saving);
+}, (error) => {
+  console.log(error);
+});
